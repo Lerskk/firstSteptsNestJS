dd64fe10f4cdcf1307f39d86b38ecf0ae959bff7
Fausto <faustoleo02@gmail.com>
2021-10-19 20:04:52 -0300

    update

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, HostParam } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
 
 @Controller({ host: 'papa.:admin.localhost' })
 export class adminControllers {
   @Get()
-  hola(@HostParam('admin') admin: string): string {
-    return admin;
+  async findAlll(): Promise<any[]> {
+    return [];
+  }
+  @Get()
+  findAll(): Observable<any[]> {
+    return of([]);
   }
 }
